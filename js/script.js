4f796137fe33f98ce45dd31e9c22732b8b64040a
HopeFaith TV <hopefaithtv@users.noreply.github.com>
2017-02-25 22:29:58 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+  'use strict';
+
+  // ========================
+  // For Fade-in effect
+  // ========================
+
+  $('html').removeClass('fade-out');
+
+  // =================
+  // Responsive videos
+  // =================
+
+  $('.post-content').fitVids();
+
+  // ===============
+  // Off Canvas menu
+  // ===============
+
+  $('.off-canvas-toggle').click(function(e) {
+    e.preventDefault();
+    $('.off-canvas-container').toggleClass('is-active');
+  });
+
+});
